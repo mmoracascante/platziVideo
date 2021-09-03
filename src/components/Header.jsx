16c7940d737c925a38f2fda1ac228a4228107510
Marco Mora <mmoracascante@gmail.com>
2021-09-02 23:00:05 -0600

    Adding all the parts of the main sight

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo from '../assets/static/logo-platzi-video-BW2.png';
+import UserIcon from '../assets/static/user-icon.png';
+
+import '../assets/styles/components/Header.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img className='header__img' src={Logo} alt='Platzi Video' />
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          <img src={UserIcon} alt='User' />
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li><a href='/'>Cuenta</a></li>
+          <li><a href='/'>Cerrar Sesión</a></li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
