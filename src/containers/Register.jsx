297ee335ee5e38d532bbd6f880e71de3477e3270
Marco Mora <mmoracascante@gmail.com>
2021-09-07 19:00:51 -0600

    Making the component player and the component Register taking the data respectly

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { registerRequest } from '../actions';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => {
+const Register = (props) => {
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInput = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  };
+
   return (
     <section className='register'>
       <section className='register__container'>
         <h2>Regístrate</h2>
-        <form className='register__container--form'>
-          <input className='input' type='text' placeholder='Nombre' />
-          <input className='input' type='text' placeholder='Correo' />
-          <input className='input' type='password' placeholder='Contraseña' />
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input
+            name='name'
+            className='input'
+            type='text'
+            onChange={handleInput}
+            placeholder='Nombre'
+          />
+          <input
+            name='email'
+            className='input'
+            type='text'
+            onChange={handleInput}
+            placeholder='Correo'
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            onChange={handleInput}
+            placeholder='Contraseña'
+          />
           <button type='submit' className='button'>Registrarme</button>
         </form>
         <Link to='/login'>Iniciar sesión</Link>
@@ -19,4 +59,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
